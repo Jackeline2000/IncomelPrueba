73edbf89c3605924d8c9431be1b9c82827b49c8e
Jackeline2000 <jacky24maldonado@gmail.com>
2021-09-08 00:38:55 -0600

    primer commit
    primer commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+import { LoginPage } from './containers/LoginPage/login';
+import { UsuariosPage } from './containers/UsuariosPage/usuarios';
+import { EmpleadoPage } from './containers/EmpleadosPage/empleado';
 
 function App() {
   return (
-    <div className="App">
-      Hola
-    </div>
+      <Router>
+          <div className="App">
+              <Switch>
+                  <Route exact path="/" component={LoginPage} />
+                  <Route exact path="/login" component={LoginPage}/>
+                  <Route exact path="/usuarios" component={UsuariosPage}/>
+                  <Route exact path="/empleados" component={EmpleadoPage}/>
+              </Switch>
+          </div>
+      </Router>
+
   );
 }
 
