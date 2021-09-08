15ebe79f8ae9001f9d2b2171f55169abf78d3163
Jackeline2000 <jacky24maldonado@gmail.com>
2021-09-07 20:34:53 -0600

    firstCommit

---
+++ src/pages/login/components/title/title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+
+    return (
+        <div>
+            soy un titulo
+
+        </div>
+    )
+};
+
+export default Title;
---
+++ src/pages/login/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const login = () => {
+
+    return (
+        <div>
+            Soy un login
+        </div>
+    )
+};
+
+export default login;
